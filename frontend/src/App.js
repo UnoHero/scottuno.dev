2b6c47ce1a98066bf1ce7ca85cc2d69fce04c106
Scott <scottunogundersen@gmail.com>
2024-05-06 12:36:12 +0200

    working language detection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,43 @@
-
+import React from "react";
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home"
+import Projects from './pages/Projects';
+import CV from './pages/CV';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Home />
-      </header>
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/"
+              element={<Home />}
+            />
+            <Route 
+              path="/projects"
+              element={<Projects />}
+            />
+            <Route 
+              path="/cv"
+              element={<CV />}
+            />
+            <Route
+              path="/contact"
+              element={<Contact />}
+            />
+            <Route
+              path="*"
+              element={<NotFound />}
+              />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
